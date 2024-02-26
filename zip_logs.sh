94c66e42d3c18aac3ed500afcfbdbba1cb23a3fa
abhijeetdrapcode <abhijeet@drapcode.com>
2024-02-26 18:08:29 +0530

    Fixed not able to find the log file

--- zip_logs.sh
+++ zip_logs.sh
@@ -8,19 +8,21 @@ output_directory="/home/acer/coding/LogFileZip"
 # if the directory does not exists it will create a new directory
 mkdir -p "$output_directory"
 
-#check and find the log files for specified date
-log_files=$(find "$log_directory" -type f -name "*$date*")
-
-if [ -z "$log_files" ]; then
-    echo "No log files found for the specified date."
+# Check and find the log file specified by the user
+if [ ! -f "$log_directory/$log_file_name" ]; then
+    echo "Specified log file does not exist."
     exit 1
 fi
 
-#finding the data with the specified date that the user entered in the terminal
+# Finding the data with the specified date in the user-specified log file
 grep "$date" "$log_directory/$log_file_name" > "$output_directory/${date}-$log_file_name-logs.txt"
 
-#zipping the log file if there is data for the specified date
-gzip "$output_directory/${date}-$log_file_name-logs.txt"
+# Zipping the log file if there is data for the specified date
+if [ -s "$output_directory/${date}-$log_file_name-logs.txt" ]; then
+    gzip "$output_directory/${date}-$log_file_name-logs.txt"
+    echo "Logs containing the specified date have been extracted and zipped successfully."
+else
+    echo "No logs found containing the specified date in the specified log file."
+fi
 
-echo "Logs containing the specified date have been extracted and zipped successfully."
 
