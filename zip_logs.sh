99224054e2f4eb8dd09b254e7a0924f3018ee422
abhijeetdrapcode <abhijeet@drapcode.com>
2024-02-26 17:41:36 +0530

    Fixed file not zipping error

--- zip_logs.sh
+++ zip_logs.sh
@@ -5,10 +5,10 @@ read -p "Enter the name of the log file (e.g., filename.log): " log_file_name
 read -p "Enter the desired date (YYYY-MM-DD): " date
 output_directory="/home/acer/coding/LogFileZip"
 
-# Ensure output directory exists
+# if the directory does not exists it will create a new directory
 mkdir -p "$output_directory"
 
-# Find log files for the specified date
+#check and find the log files for specified date
 log_files=$(find "$log_directory" -type f -name "*$date*")
 
 if [ -z "$log_files" ]; then
@@ -16,10 +16,10 @@ if [ -z "$log_files" ]; then
     exit 1
 fi
 
-# Search for logs containing the specified date in the specified log file
+#finding the data with the specified date that the user entered in the terminal
 grep "$date" "$log_directory/$log_file_name" > "$output_directory/${date}-$log_file_name-logs.txt"
 
-# Zip the log file
+#zipping the log file if there is data for the specified date
 gzip "$output_directory/${date}-$log_file_name-logs.txt"
 
 echo "Logs containing the specified date have been extracted and zipped successfully."
