54b846b3ee6b1cc797d9bdda66598e04128332b7
abhijeetdrapcode <abhijeet@drapcode.com>
2024-02-28 18:16:13 +0530

    Added mongoLog file

---
+++ mongoLog.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+log_directory="/var/log/mongodb"
+current_date=$(date +"%Y-%m-%d")
+zip_folder=$(date +"%d-%m-%Y")
+output_directory="/home/acer/coding/MongoLogZip/$zip_folder"
+zip_directory="/home/acer/coding/MongoLogZip"
+zip_file="$zip_directory/mongodb-logs-$zip_folder.zip"
+
+mkdir -p "$output_directory"
+
+log_files=$(find "$log_directory" -type f -name "*.log")
+
+if [ -z "$log_files" ]; then
+    echo "There are no log files for mongodb in this folder."
+    exit 1
+fi
+
+for log_file in $log_files; do
+
+    grep "$current_date" "$log_file" > "$output_directory/$(basename "$log_file")-${current_date}.log"
+    
+    if [ -s "$output_directory/$(basename "$log_file")-${current_date}.log" ]; then
+        echo "Logs containing the current date from $(basename "$log_file") have been extracted successfully."
+    else
+        rm -f "$output_directory/$(basename "$log_file")-${current_date}.log"
+    fi
+done
+
+zip -j "$zip_file" "$output_directory"/*.log
+
+
+rm -r "$output_directory"
+
+echo "MongoDB logs for the current date have been zipped into $zip_file."
