bf91750ad408e754c8fe5c475baf49700d2142d5
abhijeetdrapcode <abhijeet@drapcode.com>
2024-03-01 12:35:31 +0530

    modified linuxOs.sh files so that it can extract dates from multiple files

--- linuxOS.sh
+++ linuxOS.sh
@@ -1,28 +1,41 @@
 #!/bin/bash
 
-log_file="/var/log/syslog"
-current_date=$(date +"%b %d") 
-zip_folder=$(date +"%d-%m-%Y")
-output_directory="/home/acer/coding/LogFileZip/$zip_folder"
-zip_directory="/home/acer/coding/LogFileZip"
+log_files=("/var/log/syslog" "/var/log/auth.log" "/var/log/kern.log")
+zip_folder=$(date +"%Y-%m-%d")
+output_directory="/home/acer/coding/OsFileZip/$zip_folder"
+zip_directory="/home/acer/coding/OsFileZip"
 zip_file="$zip_directory/logs-$zip_folder.zip"
 
 mkdir -p "$output_directory"
 
-if [ ! -f "$log_file" ]; then
-    echo "The log file $log_file does not exist."
-    exit 1
-fi
+today_commands_file="/tmp/today_commands.txt"
+history_output=$(history | grep "$(date +"%Y-%m-%d")")
+echo "Today's commands from history:"
+echo "$history_output" > "$today_commands_file"
+cat "$today_commands_file"
 
-grep "$current_date" "$log_file" > "$output_directory/${current_date}-syslog-logs.txt"
+for log_file in "${log_files[@]}"; do
+    current_date=$(date +"%b %e")
 
-if [ -s "$output_directory/${current_date}-syslog-logs.txt" ]; then
-    echo "Logs containing the current date from syslog have been extracted successfully."
-else
-    rm -f "$output_directory/${current_date}-syslog-logs.txt"
-    echo "No logs containing the current date found in syslog."
-fi
+    if [ ! -f "$log_file" ]; then
+        echo "The log file $log_file does not exist."
+        continue
+    fi
 
-zip -j "$zip_file" "$output_directory"/*
+    grep "$current_date" "$log_file" > "$output_directory/${current_date}-$(basename $log_file)-logs.txt"
+
+    if [ -s "$output_directory/${current_date}-$(basename $log_file)-logs.txt" ]; then
+        echo "Logs containing the current date from $(basename $log_file) have been extracted successfully."
+    else
+        rm -f "$output_directory/${current_date}-$(basename $log_file)-logs.txt"
+        echo "No logs containing the current date found in $(basename $log_file)."
+    fi
+done
+
+zip -j "$zip_file" "$output_directory"/* "$today_commands_file"
+
+
 rm -r "$output_directory"
-echo "Logs for the current date from syslog have been zipped into $zip_file."
+rm "$today_commands_file"
+
+echo "Logs for the current date from syslog, auth.log, and kern.log along with today's commands have been zipped into $zip_file."
