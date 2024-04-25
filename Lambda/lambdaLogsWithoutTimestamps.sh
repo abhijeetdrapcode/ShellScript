2d3432c9b32780114bba755aecedf07c778ffb95
abhijeetdrapcode <abhijeet@drapcode.com>
2024-04-25 18:11:23 +0530

    Added LambdaLog file with timestamp

--- Lambda/lambdaLogsWithoutTimestamps.sh
+++ Lambda/lambdaLogsWithoutTimestamps.sh
@@ -1,12 +1,4 @@
-
 #!/bin/bash
-
-# Function to add 5 hours and 30 minutes to the current time
-add_time() {
-  local current_time=$(date +%s)
-  local adjusted_time=$((current_time + 5*3600 + 30*60))
-  date -d "@$adjusted_time" "+%Y-%m-%d %H:%M:%S"
-}
 
 list_log_streams() {
   function_name="$1"
@@ -17,12 +9,6 @@ fetch_log_events() {
   function_name="$1"
   log_stream_name="$2"
   aws logs get-log-events --log-group-name "/aws/lambda/$function_name" --log-stream-name "$log_stream_name" --output json
-}
-
-convert_timestamp() {
-  local milliseconds="$1"
-  local adjusted_time=$(($milliseconds + 5*3600*1000 + 30*60*1000))  
-  TZ="Asia/Kolkata" date -d "@$((adjusted_time / 1000))" "+%Y-%m-%d %H:%M:%S %Z"
 }
 
 lambda_functions=$(aws lambda list-functions --query 'Functions[*].FunctionName')
@@ -49,16 +35,14 @@ read -p "Enter the name of the log stream you want to download logs from: " log_
 echo "Fetching all log events for Log Stream: $log_stream_name"
 log_events=$(fetch_log_events "$function_name" "$log_stream_name")
 
-log_events_converted=$(echo "$log_events" | jq '.events[] | .timestamp |= . / 1000 | .timestamp |= strftime("%Y-%m-%d %H:%M:%S %Z")')
-
 echo "Log Events for Log Stream $log_stream_name:"
-echo "$log_events_converted"
+echo "$log_events"
 echo
 
 read -p "Do you want to download all log events for Log Stream $log_stream_name? (yes/no): " download_choice
 
 if [[ "$download_choice" == "yes" || "$download_choice" == "y" ]]; then
   log_stream_name_cleaned="${log_stream_name//\//_}"
-  echo "$log_events_converted" > "${function_name}-${log_stream_name_cleaned}-logs.json"
-  echo "Log events downloaded to: ${function_name}-${log_stream_name_cleaned}-logs.json in the current directory."
+  echo "$log_events" > "${function_name}-logs.json"
+  echo "Log events downloaded to: ${function_name}-logs.json in the current directory."
 fi
