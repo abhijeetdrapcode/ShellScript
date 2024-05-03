75e18199a7c5f3993015dcc68eb68f1bcfed6c8f
abhijeetdrapcode <abhijeet@drapcode.com>
2024-05-03 11:09:24 +0530

    Added ip in the linux command history file

--- SeperateFiles/linuxCommands.sh
+++ SeperateFiles/linuxCommands.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 history_file=~/.bash_history
-output_directory="/home/acer/coding/zipFolder/LinuxCommand"
+output_directory="/home/acer/zipFolder/LinuxCommand"
 
 today=$(date +"%Y-%m-%d")
 zip_folder=$(date +"%d-%m-%Y")
@@ -11,6 +11,7 @@ zip_file="$output_directory/$zip_folder.zip"
 
 mkdir -p "$output_directory"
 user=$(whoami)
+ip_address=$(hostname -I | awk '{print $1}')
 matched_lines=()
 
 while IFS= read -r line; do
@@ -18,7 +19,7 @@ while IFS= read -r line; do
     if [[ "$timestamp" =~ ^[0-9]+$ ]]; then 
         date_time=$(date -d "@$timestamp" +"%Y-%m-%d %H:%M:%S")
         if [[ "${date_time:0:10}" == "$today" ]]; then  
-            matched_lines+=("[$date_time] User: $user | Command: $prev_line")
+            matched_lines+=("[$date_time] User: $user |  IP: $ip_address |Command: $prev_line")
         fi
     else
         prev_line="$line"
